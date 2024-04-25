db894c29c635f209ca83506156ff14bfc908664f
eazavorina <zav.elena.int@gmail.com>
2024-04-25 17:36:04 +0300

    added moving to login page

--- test_main_page.py
+++ test_main_page.py
@@ -9,7 +9,6 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
-    login_page.open()
     login_page.should_be_login_page()
 
 
